bf61ff23bc12b1aa346ed99b29121f56388eb289
Verlemion <pyankov02@gmail.com>
2017-01-12 16:34:24 +0600

    More security

--- lib.php
+++ lib.php
@@ -15,7 +15,7 @@ function checkGJP($gjp, $accountID) {
 }
 
 function sqlTrim($data) {
-	return str_replace(array("'", "(", ")", "~", ":", "|", "#"), "", htmlspecialchars($data));
+	return str_replace(array("'", "(", ")", "~", ":", "|", "#"), "", htmlspecialchars($data, ENT_QUOTES));
 }
 
 function makeTime($datetime) {
