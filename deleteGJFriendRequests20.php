06a366d1e475ead103d67246982d53aaca390c55
Verlemion <pyankov02@gmail.com>
2017-01-12 16:25:56 +0600

    Friend request remover

---
+++ deleteGJFriendRequests20.php
@@ -0,0 +1,23 @@
+<?
+include "connection.php";
+require "lib.php";
+
+$accountID = sqlTrim($_POST["accountID"]);
+$gjp = sqlTrim($_POST["gjp"]);
+$targetAccountID = sqlTrim($_POST["targetAccountID"]);
+$isSender = sqlTrim($_POST["isSender"]);
+
+if(checkGJP($gjp, $accountID)) {
+	if($targetAccountID != "0") {
+		if($isSender != "0") {
+			$q = $db->prepare("DELETE FROM frRequests WHERE accountID = '$accountID' AND targetID = '$targetAccountID'");
+			$q->execute();
+			exit("1");
+		} else {
+			$q = $db->prepare("DELETE FROM frRequests WHERE targetID = '$accountID' AND accountID = '$targetAccountID'");
+			$q->execute();
+			exit("1");
+		}
+	} 
+} else exit("-1");
+?>
