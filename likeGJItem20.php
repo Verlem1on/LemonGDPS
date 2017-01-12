1db09fc686535b3fa49908a1fd7992e83121a731
Verlemion <pyankov02@gmail.com>
2017-01-12 19:59:01 +0600

    Now you can like comments

--- likeGJItem20.php
+++ likeGJItem20.php
@@ -18,8 +18,15 @@ switch ($type) {
 		exit("1");
 		break;
 	
-	default:
-		# code...
+	case '2':
+		# Comment
+		$q = $db->prepare("SELECT * FROM comments WHERE commentID = '$itemID'");
+		$q->execute();
+		$r = $q->fetch(PDO::FETCH_ASSOC);
+		if($like == "1") $r["likes"] += 1; else $r["likes"] -= 1;
+		$q = $db->prepare("UPDATE comments SET likes = '".$r["likes"]."' WHERE commentID = '$itemID'");
+		$q->execute();
+		exit("1");
 		break;
 }
 ?>
