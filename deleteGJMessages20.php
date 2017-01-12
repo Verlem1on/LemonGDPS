b312b0b2bfe81ec86755105de4a27cc858b29730
Verlemion <pyankov02@gmail.com>
2017-01-12 15:49:08 +0600

    Now you can delete messages

---
+++ deleteGJMessages20.php
@@ -0,0 +1,36 @@
+<?
+include "connection.php";
+require "lib.php";
+
+$accountID = sqlTrim($_POST["accountID"]);
+$gjp = sqlTrim($_POST["gjp"]);
+$messageID = sqlTrim($_POST["messageID"]);
+$isSender = sqlTrim($_POST["isSender"]);
+$messages = sqlTrim($_POST["messages"]);
+
+if(checkGJP($gjp, $accountID)) {
+	if($messageID != "") {
+		$q = $db->prepare("DELETE FROM messages WHERE messageID = '$messageID'");
+		$q->execute();
+		exit("1");
+	} 
+
+	if($messages != "") {
+		if($isSender != "") {
+			$mArray = explode(',', $messages);
+			foreach($mArray as $m) {
+				$q = $db->prepare("DELETE FROM messages WHERE messageID = '$m' AND accountID = '$accountID'");
+				$q->execute();
+			}
+			exit("1");
+		} else {
+			$mArray = explode(',', $messages);
+			foreach($mArray as $m) {
+				$q = $db->prepare("DELETE FROM messages WHERE messageID = '$m'");
+				$q->execute();
+			}
+			exit("1");
+		}
+	}
+} else exit("-1");
+?>
