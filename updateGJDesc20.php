24513827fb7ad322e69394627f692db1ac3e0592
Verlemion <pyankov02@gmail.com>
2017-01-12 20:22:53 +0600

    Fixed exploit

--- updateGJDesc20.php
+++ updateGJDesc20.php
@@ -8,7 +8,10 @@ $levelDesc = sqlTrim($_POST["levelDesc"]);
 $levelID = sqlTrim($_POST["levelID"]);
 
 if(checkGJP($gjp, $accountID)) {
-	$q = $db->prepare("UPDATE levels SET `desc` = '$levelDesc' WHERE levelID = '$levelID'");
+	$q = $db->prepare("SELECT * FROM users WHERE accountID = '$accountID'");
+	$q->execute();
+	$r = $q->fetch(PDO::FETCH_ASSOC);
+	$q = $db->prepare("UPDATE levels SET `desc` = '$levelDesc' WHERE levelID = '$levelID' AND userID ='".$r["userID"]."'");
 	$q->execute();
 	exit("1");
 } else exit("-1");
