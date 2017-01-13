2f8a4e9a2b5fc45f50efef686093ab9c0021437a
Verlemion <pyankov02@gmail.com>
2017-01-13 14:54:11 +0600

    Anticheat added

--- updateGJUserScore20.php
+++ updateGJUserScore20.php
@@ -26,6 +26,41 @@ $iconType = sqlTrim($_POST["iconType"]);
 if(disabled($accountID)) exit("-1");
 
 if (checkGJP($gjp, $accountID)) {
+	# Anticheat
+
+	$q = $db->prepare("SELECT * FROM levels");
+	$q->execute();
+	$levels = $q->fetchAll();
+
+	$q = $db->prepare("SELECT * FROM mappacks");
+	$q->execute();
+	$mp = $q->fetchAll();
+
+	$maxStars = 175;
+	$maxCoins = 60;
+	$maxUC = 0;
+	$maxDemons = 3;
+
+	for($i = 0; $i < count($levels); $i++) {
+		$level = $levels[$i];
+		$maxStars += $level["stars"];
+		$maxDemons += $level["demon"];
+		if($level["verifiedCoins"] == "1") {
+			$maxUC += $level["coins"];
+		}
+	}
+
+	for($i = 0; $i < count($mp); $i++) {
+		$m = $mp[$i];
+		$maxStars += $m["stars"];
+		$maxCoins += $m["coins"];
+	}
+
+	if($stars > $maxStars || $coins > $maxCoins || $userCoins > $maxUC || $demons > $maxDemons) {
+		$q = $db->prepare("UPDATE accounts SET banned = '1' WHERE accountID = :a");
+		$q->execute(array('a' => $accountID));
+	}
+
 	$a = $db->prepare("SELECT * FROM accounts WHERE accountID = '$accountID'");
 	$a->execute();
 	$r1 = $a->fetch(PDO::FETCH_ASSOC);
