758d48bd5558bfca7342c9699d4d4a87b71d082d
Verlemion <pyankov02@gmail.com>
2017-01-13 14:02:19 +0600

    Account comments likes added

--- likeGJItem20.php
+++ likeGJItem20.php
@@ -28,5 +28,16 @@ switch ($type) {
 		$q->execute();
 		exit("1");
 		break;
+
+	case '3':
+		# Acc Comment
+		$q = $db->prepare("SELECT * FROM comments WHERE commentID = :i AND accountComment = '1'");
+		$q->execute(array('i' => $itemID));
+		$r = $q->fetch(PDO::FETCH_ASSOC);
+		if($like == "1") $r["likes"] += 1; else $r["likes"] -= 1;
+		$q = $db->prepare("UPDATE comments SET likes = '".$r["likes"]."' WHERE commentID = '$itemID'");
+		$q->execute();
+		exit("1");
+		break;
 }
 ?>
